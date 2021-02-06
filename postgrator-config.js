f089c381e6ba5cd51e7b9bf0549b06616eed646f
nick <nlomba1@gmail.com>
2021-02-05 20:18:39 -0500

    version control

--- postgrator-config.js
+++ postgrator-config.js
@@ -1,7 +1,8 @@
 require("dotenv").config();
 const pg = require("pg");
 
-pg.defaults.ssl = process.env.NODE_ENV === "production";
+pg.defaults.ssl =
+  process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false;
 
 module.exports = {
   migrationDirectory: "migrations",
@@ -10,5 +11,4 @@ module.exports = {
     process.env.NODE_ENV === "test"
       ? process.env.TEST_DATABASE_URL
       : process.env.DATABASE_URL,
-  ssl: process.env.NODE_ENV === "production" ? true : false,
 };
