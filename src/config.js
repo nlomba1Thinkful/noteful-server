a3ce3fbc6c4e3059f564646d72960d38f343309f
nick <nlomba1@gmail.com>
2021-02-07 17:41:07 -0500

    db url change

--- src/config.js
+++ src/config.js
@@ -1,7 +1,8 @@
-require('dotenv').config()
+require("dotenv").config();
 
 module.exports = {
-    PORT: process.env.PORT || 8000,
-    NODE_ENV: process.env.NODE_ENV || 'development',
-    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://dunder_mifflin@localhost/noteful',
-}
+  PORT: process.env.PORT || 8000,
+  NODE_ENV: process.env.NODE_ENV || "development",
+  DATABASE_URL:
+    process.env.DATABASE_URL || "postgresql://283801@localhost/notefulv2",
+};
