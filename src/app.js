444c17d14dcb1eac55c8675339bbc1de0430364f
nick <nlomba1@gmail.com>
2021-02-08 12:06:11 -0500

    origin add

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ const app = express();
 const morganOption = NODE_ENV === "production" ? "tiny" : "common";
 app.use(morgan(morganOption));
 app.use(helmet());
-app.use(cors());
+app.use(cors({ origin: "https://noteful-client-qm47q6rdg.vercel.app/" }));
 
 app.use("/folders", FoldersRouter);
 app.use("/notes", NotesRouter);
